15aab7eb4ee12003c287d8a51572a20b844e84d5
gaurav-lala <gaurav.lala@hotmail.co.uk>
2018-11-20 08:41:25 +0000

    comment added

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller class for the 
+ * Controller class for word processing requests
  * 
  * @author Gaurav
  *
